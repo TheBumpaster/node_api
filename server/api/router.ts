aed255708c036e2dbd2f7aa514fc604787429cc7
TheBumpaster <hadzicismar67@gmail.com>
2021-01-10 14:24:24 +0100

    Minor update to dev

--- server/api/router.ts
+++ server/api/router.ts
@@ -12,55 +12,58 @@ class ExpressRouter {
         app.use('/api/v1/', new SystemRouter().getRouter());
         app.use('/api/v1/users/', new UserRouter().getRouter());
 
-        /**
-         * Searches through project files for API definitions and generates json
-         * @param request
-         * @param response
-         */
-        app.use('/api/v1/docs', (request, response) => {
-            glob(join(__dirname, './v1/**/*.ts'), (err: unknown, urls: unknown[]) => {
-                const options = {
-                    apis: urls,
-                    swaggerDefinition: {
-                        info: {
-                            description: `Main API Documentation`,
-                            title: 'Swagger API Docs',
-                            version: '1.0.0',
+        // if deployed for development
+        if (process.env.NODE_ENV === 'development') {
+            /**
+             * Searches through project files for API definitions and generates json
+             * @param request
+             * @param response
+             */
+            app.use('/api/v1/docs', (request, response) => {
+                glob(join(__dirname, './v1/**/*.js'), (err: unknown, urls: unknown[]) => {
+                    const options = {
+                        apis: urls,
+                        swaggerDefinition: {
+                            info: {
+                                description: `Main API Documentation`,
+                                title: 'Swagger API Docs',
+                                version: '1.0.0',
+                            },
+                            openapi: '3.0.0',
                         },
-                        openapi: '3.0.0',
-                    },
-                };
-                response.setHeader('Content-Type', 'application/json');
-                response.send(swaggerJSDoc(options));
+                    };
+                    response.setHeader('Content-Type', 'application/json');
+                    response.send(swaggerJSDoc(options));
+                });
             });
-        });
 
-        /**
-         * Create UI page for swagger
-         */
-        const uriV1 =
-            process.env.NODE_ENV !== 'development'
-                ? `${process.env.HOST}/api/v1/docs`
-                : `${process.env.HOST}:${process.env.PORT}/api/v1/docs`;
-        app.use(
-            '/docs',
-            swaggerUi.serve,
-            swaggerUi.setup(null, {
-                swaggerOptions: {
-                    urls: [
-                        {
-                            url: uriV1,
-                            name: 'V1 API Specification',
-                        },
-                    ],
-                },
-            }),
-        );
+            /**
+             * Create UI page for swagger
+             */
+            const uriV1 =
+                process.env.NODE_ENV !== 'development'
+                    ? `${process.env.HOST}/api/v1/docs`
+                    : `${process.env.HOST}:${process.env.PORT}/api/v1/docs`;
+            app.use(
+                '/docs',
+                swaggerUi.serve,
+                swaggerUi.setup(null, {
+                    swaggerOptions: {
+                        urls: [
+                            {
+                                url: uriV1,
+                                name: 'V1 API Specification',
+                            },
+                        ],
+                    },
+                }),
+            );
 
-        /**
-         * Create UI Page for typedoc
-         */
-        app.use('/typedocs', express.static(join(__dirname, '../../docs/')));
+            /**
+             * Create UI Page for typedoc
+             */
+            app.use('/typedocs', express.static(join(__dirname, '../../docs/')));
+        }
 
         // Any not defined path request
         app.use(function (req: Request, res: Response, next: NextFunction) {
